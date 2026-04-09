e39042954b2b07e617cc6fcc11fe076697831ea5
Adin Refqi <adin.refqi@gmail.com>
2026-04-09 10:22:09 +0700

    Restorasi UI Sumatif IV dan Perbaikan Script

--- push_sekarang.bat
+++ push_sekarang.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Melakukan commit dan push ke adinrefqi/sumatif3...
 git add .
-git commit -m "Update terbaru aplikasi Sumatif 3"
+git commit -m "Restorasi UI Sumatif IV dan Perbaikan Script"
 git push origin main
 echo.
 echo Proses selesai! Tekan sembarang tombol untuk menutup layar ini.
